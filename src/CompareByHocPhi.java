70168c69237502c93e32905178f4bbb09a58f165
Admin <Admin@DESKTOP-Q6N4J46>
2023-05-12 16:16:58 +0700

    NguyenMinhDucUpload

---
+++ src/CompareByHocPhi.java
@@ -0,0 +1,10 @@
+
+import java.util.Comparator;
+
+public class CompareByHocPhi implements Comparator<HocVienDH>{
+
+    @Override
+    public int compare(HocVienDH o1, HocVienDH o2) {
+        return Double.compare(o1.hocPhi(), o2.hocPhi());
+    }
+}
